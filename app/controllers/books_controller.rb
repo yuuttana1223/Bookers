588612fb54e4a16f173ccdd7481a39fc2048424d
EC2 Default User <ec2-user@ip-172-31-91-96.ec2.internal>
2021-02-18 03:44:38 +0000

    fix home index

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,22 @@
+class BooksController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def update
+  end
+
+  def edit
+  end
+
+  def create
+  end
+
+  def destroy
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
   root to: 'home#index'
-  get 'home/index'
+  resources :books
 end
