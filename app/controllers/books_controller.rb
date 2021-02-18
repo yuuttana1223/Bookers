290c6b2c22d523f9b5291c687c85c47b286578a2
EC2 Default User <ec2-user@ip-172-31-91-96.ec2.internal>
2021-02-18 07:13:06 +0000

    add success message and error messages

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,8 +1,10 @@
 class BooksController < ApplicationController
+  before_action :set_books, only: [:index, :create]
   before_action :set_book, only: [:show, :edit, :update, :destroy]
+  before_action :set_update_btn_name, only: [:edit, :update]
+  before_action :set_create_btn_name, only: [:index, :create]
 
   def index
-    @books = Book.all
     @book = Book.new
   end
 
@@ -10,27 +12,49 @@ class BooksController < ApplicationController
   end
 
   def create
-    book = Book.new(book_params)
-    book.save
-    redirect_to book_path(book.id)
+    @book = Book.new(book_params)
+    if @book.save
+      flash[:success] = "Book was successfully created."
+      redirect_to book_path(@book.id)
+    else
+      render "index"
+    end
   end
 
   def edit
   end
 
   def update
-    @book.update(book_params)
-    redirect_to book_path(@book.id)
+    if @book.update(book_params)
+      flash[:success] = "Book was successfully updated."
+      redirect_to book_path(@book.id)
+    else
+      render "edit"
+    end
   end
 
   def destroy
     @book.destroy
+    flash[:success] = "Book was successfully destroyed."
     redirect_to books_path
   end
 
   private
+    def set_books
+      @books = Book.all
+    end
+
     def set_book
       @book = Book.find(params[:id])
+    end
+
+    def set_update_btn_name
+      # 送信ボタンの文字
+      @submit_name = "Update Book"
+    end
+
+    def set_create_btn_name
+      @submit_name = "Create Book"
     end
 
     def book_params
