f038cd40c3fd2b489668827614ebdf907bd8e66b
EC2 Default User <ec2-user@ip-172-31-91-96.ec2.internal>
2021-02-18 04:03:54 +0000

    add book show

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,4 +1,6 @@
 class BooksController < ApplicationController
+  before_action :set_book, only: [:show]
+
   def index
     @books = Book.all
   end
@@ -20,4 +22,9 @@ class BooksController < ApplicationController
 
   def destroy
   end
+
+  private
+    def set_book
+      @book = Book.find(params[:id])
+    end
 end
