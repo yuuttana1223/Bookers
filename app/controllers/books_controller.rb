5180197c49c042ab5ba3b3d280c3a121c5956bd3
yuuttana1223 <yutanadreamfilel.seed@gmail.com>
2021-03-08 16:06:23 +0900

    [Clean] flash[:success]をflash[:notice]に変更

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -14,7 +14,7 @@ class BooksController < ApplicationController
   def create
     @book = Book.new(book_params)
     if @book.save
-      flash[:success] = "Book was successfully created."
+      flash[:notice] = "Book was successfully created."
       redirect_to book_path(@book.id)
     else
       @books = Book.all
@@ -29,7 +29,7 @@ class BooksController < ApplicationController
   def update
     @book = Book.find(params[:id])
     if @book.update(book_params)
-      flash[:success] = "Book was successfully updated."
+      flash[:notice] = "Book was successfully updated."
       redirect_to book_path(@book.id)
     else
       render "edit"
@@ -39,7 +39,7 @@ class BooksController < ApplicationController
   def destroy
     @book = Book.find(params[:id])
     @book.destroy
-    flash[:success] = "Book was successfully destroyed."
+    flash[:notice] = "Book was successfully destroyed."
     redirect_to books_path
   end
 
