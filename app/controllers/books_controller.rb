ab77d900cf8a9689c6719390a24af0c5c3a9acbc
yuuttana1223 <yutanadreamfilel.seed@gmail.com>
2021-03-08 15:57:21 +0900

    [Clean] books_controllerのセッターをなくして直接書き込む

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,14 +1,14 @@
 class BooksController < ApplicationController
-  before_action :set_books, only: [:index, :create]
-  before_action :set_book, only: [:show, :edit, :update, :destroy]
   before_action :set_update_btn_name, only: [:edit, :update]
   before_action :set_create_btn_name, only: [:index, :create]
 
   def index
+    @books = Book.all
     @book = Book.new
   end
 
   def show
+    @book = Book.find(params[:id])
   end
 
   def create
@@ -17,14 +17,17 @@ class BooksController < ApplicationController
       flash[:success] = "Book was successfully created."
       redirect_to book_path(@book.id)
     else
+      @books = Book.all
       render "index"
     end
   end
 
   def edit
+    @book = Book.find(params[:id])
   end
 
   def update
+    @book = Book.find(params[:id])
     if @book.update(book_params)
       flash[:success] = "Book was successfully updated."
       redirect_to book_path(@book.id)
@@ -34,30 +37,24 @@ class BooksController < ApplicationController
   end
 
   def destroy
+    @book = Book.find(params[:id])
     @book.destroy
     flash[:success] = "Book was successfully destroyed."
     redirect_to books_path
   end
 
   private
-    def set_books
-      @books = Book.all
-    end
 
-    def set_book
-      @book = Book.find(params[:id])
-    end
+  def set_update_btn_name
+    # 送信ボタンの文字
+    @submit_name = "Update Book"
+  end
 
-    def set_update_btn_name
-      # 送信ボタンの文字
-      @submit_name = "Update Book"
-    end
+  def set_create_btn_name
+    @submit_name = "Create Book"
+  end
 
-    def set_create_btn_name
-      @submit_name = "Create Book"
-    end
-
-    def book_params
-      params.require(:book).permit(:title, :body)
-    end
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
 end
